e99e1dfbb713ab61f35f7fe5a64a5de4491679b6
Tomasz Orłowski <orlosmistrz123@bitbucket.org>
2020-02-10 21:52:54 +0100

    deployent scripts

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ docker push torlowski/multi-client:latest
 docker push torlowski/multi-server:latest 
 docker push torlowski/multi-worker:latest
 docker push torlowski/multi-client:$SHA 
-docker push torlowski/multi-server:$SHA 
-docker push torlowski/multi-worker:$SHA 
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=torlowski/multi-server:$SHA
-kubectl set image deployments/client-deployment server=torlowski/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=torlowski/multi-worker:$SHA
+#docker push torlowski/multi-server:$SHA 
+#docker push torlowski/multi-worker:$SHA 
+#kubectl apply -f k8s
+#kubectl set image deployments/server-deployment server=torlowski/multi-server:$SHA
+#kubectl set image deployments/client-deployment server=torlowski/multi-client:$SHA
+#kubectl set image deployments/worker-deployment server=torlowski/multi-worker:$SHA
